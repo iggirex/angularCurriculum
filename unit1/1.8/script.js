1cacedd8ffef957b6774746e57c971a2e11df124
Iggi <iggirex@gmail.com>
2016-10-13 22:21:57 -0600

    bingo working to 11, and reset buttn

---
+++ unit1/1.8/script.js
@@ -0,0 +1,45 @@
+var app = angular.module("myApp", [])
+function alert(){
+  console.log("yo alert works floarting out on its own")
+}
+app.controller("myCtrl", function($scope){
+  $scope.view = {};
+  $scope.view.number= 4;
+  $scope.pickRandomNumber = function(){
+    $scope.view.number = Math.floor(Math.random() * 10) +1;
+    console.log('getting CA')
+  };
+});
+app.controller("reverseCtrl", function($scope){
+  $scope.reverse = function(){
+    var input = $scope.viewRev.stringReverse;
+    $scope.viewRev.stringReverse = input.split("").reverse().join("");
+  };
+});
+app.controller("pongCtrl", function($scope){
+  $scope.pong = {};
+  $scope.pong.leftScore = 0;
+  $scope.pong.rightScore = 0;
+  $scope.reset = function(){
+    console.log("this workzzzzz");
+  };
+  $scope.leftScoreIncrease = function(){
+    if($scope.pong.leftScore < 11){
+    $scope.pong.leftScore++
+  } else{
+    $scope.pong.leftScore = 11;
+  };
+};
+  $scope.rightScoreIncrease = function(){
+    if($scope.pong.rightScore < 11){
+    $scope.pong.rightScore++
+  } else{
+    $scope.pong.rightScore = 11;
+    document.getElementByClassName("right").addClass("won")
+  };
+};
+  $scope.reset = function(){
+    $scope.pong.leftScore = 0;
+    $scope.pong.rightScore = 0;
+  };
+});
