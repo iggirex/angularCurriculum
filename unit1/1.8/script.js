1f19788500f49049fcc400684ef33f53b3bf1c94
Iggi <iggirex@gmail.com>
2016-10-13 23:07:07 -0600

    funky behavior with css classes and font sizes

--- unit1/1.8/script.js
+++ unit1/1.8/script.js
@@ -17,6 +17,8 @@ app.controller("reverseCtrl", function($scope){
   };
 });
 app.controller("pongCtrl", function($scope){
+  var counter = 0;
+  var serveStyle;
   $scope.pong = {};
   $scope.pong.leftScore = 0;
   $scope.pong.rightScore = 0;
@@ -24,22 +26,46 @@ app.controller("pongCtrl", function($scope){
     console.log("this workzzzzz");
   };
   $scope.leftScoreIncrease = function(){
-    if($scope.pong.leftScore < 11){
+    if($scope.pong.leftScore < 10){
     $scope.pong.leftScore++
+    counter++
+      if(counter % 3 === 0){
+        $scope.pong.leftServeStyle = false;
+        $scope.pong.rightServeStyle = true;
+      } else {
+        $scope.pong.leftServeStyle = true;
+        $scope.pong.rightServeStyle = false;
+      }
   } else{
     $scope.pong.leftScore = 11;
+    $scope.pong.leftWon = true
+    $scope.pong.rightLose = true;
   };
 };
   $scope.rightScoreIncrease = function(){
-    if($scope.pong.rightScore < 11){
+    if($scope.pong.rightScore < 10){
     $scope.pong.rightScore++
+    counter++
+    if(counter % 3 === 0){
+      $scope.pong.rightServeStyle = true;
+      $scope.pong.leftServeStyle = false;
+    } else {
+      $scope.pong.rightServeStyle = false;
+      $scope.pong.leftServeStyle = true;
+    }
   } else{
     $scope.pong.rightScore = 11;
-    document.getElementByClassName("right").addClass("won")
+    $scope.pong.rightWon = true
+    $scope.pong.leftLose = true;
   };
 };
   $scope.reset = function(){
     $scope.pong.leftScore = 0;
     $scope.pong.rightScore = 0;
+    $scope.pong.rightWon = false;
+    $scope.pong.leftLose = false;
+    $scope.pong.leftWon = false;
+    $scope.pong.rightLose = false;
+    counter = 0;
   };
 });
