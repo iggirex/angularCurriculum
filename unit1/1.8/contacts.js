11dae764ae91ace2e2d0acacef3cfa0be9b0e7ef
Iggi <iggirex@gmail.com>
2016-10-14 15:21:11 -0600

    checking out quickly

---
+++ unit1/1.8/contacts.js
@@ -0,0 +1,14 @@
+var app = angular.module("contacts", []);
+
+app.controller("myCtrl", function($scope){
+  $scope.form = {};
+  $scope.collection= [];
+  $scope.submitEntry = function(form){
+    var thisOneThing = {name: form.name, email: form.email, phone: form.phone};
+    $scope.collection.push(thisOneThing);
+  };
+  $scope.clearOut = function(form){
+    $scope.form= null;
+  };
+  $scope.
+});
